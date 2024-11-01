c6d4633c06610645bdeb29bf7b88696cc162cb90
wilianAlbrecht <wilian0012@hotmail.com>
2024-11-01 08:08:36 -0400

    create project

---
+++ src/main/java/info/wilian/ey_dy_calculator/EyDyCalculatorApplication.java
@@ -0,0 +1,13 @@
+package info.wilian.ey_dy_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyDyCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyDyCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/wilian/ey_dy_calculator/EyDyCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package info.wilian.ey_dy_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyDyCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
